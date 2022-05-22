c0a3736cc366a15755257cd3a4b5c4b2f3a45574
Nahueljp96 <nahuel5831@gmail.com>
2022-05-21 22:44:03 -0300

    asd

--- Formulario_datos_Personales/index.php
+++ Formulario_datos_Personales/index.php
@@ -11,7 +11,7 @@
 <body>
     <main class="container">
         <div class="row">
-            <div class="col-12 text-center">
+            <div class="col-12 text-center py-3">
                 <h1>Formulario</h1>
             </div>
         </div>
